c61817cebfea2009df23882d7621860ac27596c0
Ondrej Fuhrer <ondrejfuhrer@users.noreply.github.com>
2016-03-02 12:23:14 +0100

    Rails admin setup

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_presents_session'
+Rails.application.config.session_store :cookie_store, key: '_rocket_library_session'
