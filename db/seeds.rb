c831deff17d7412c906e2e894dd5d3d54d9dc968
Georgios Katsanos <gkatsanos@gmail.com>
2016-03-02 12:26:45 +0100

    Seed data, initial styling, removing unneeded CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,5 +18,51 @@ User.new do |u|
   u.email = 'second@family.com'
 end
 
+Book.create do |b|
+  b.name = 'To Kill a Mockingbird'
+  b.author = 'Harper Lee'
+  b.sku = '12345'
+end
 
+Book.create do |b|
+  b.name = 'How to learn Chinese in 135 years'
+  b.author = 'John Smith'
+  b.sku = '12346'
+end
+
+Book.create do |b|
+  b.name = 'Whatever'
+  b.author = 'Harper Lee Johnson'
+  b.sku = '12347'
+end
+
+Book.create do |b|
+  b.name = 'Hunting and Fishing'
+  b.author = 'Elvis Thomson'
+  b.sku = '12348'
+end
+
+Book.create do |b|
+  b.name = 'Software Engineering for newbies'
+  b.author = 'Georgios Katsanos'
+  b.sku = '12349'
+end
+
+Book.create do |b|
+  b.name = 'Ruby for beginners'
+  b.author = 'Dmitry Filatov'
+  b.sku = '12350'
+end
+
+Book.create do |b|
+  b.name = 'C++ for advanced users'
+  b.author = 'John Anderson'
+  b.sku = '12351'
+end
+
+Book.create do |b|
+  b.name = 'Fates and Furies Very long title here for experimenting with the layout'
+  b.author = 'Lauren Groff Van Thomson Pierson Writerson'
+  b.sku = '12352'
+end
 
