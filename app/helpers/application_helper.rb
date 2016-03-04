93018b771f7bb927515ea38c190bfd3a1588fda7
Georgios Katsanos <gkatsanos@gmail.com>
2016-03-04 10:41:31 +0100

    Moving logic from template to a Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def get_flash_message_class(type)
+    case type
+      when 'notice'
+        'alert alert-success'
+      else
+        'alert alert-danger'
+    end
+  end
 end
+
